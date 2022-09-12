4f7720c0b3b2ba1d7967622fe28547ad95843f88
huudiem25 <satthucrisi1@gmail.com>
2022-09-12 18:58:26 +0700

    orc

---
+++ Tesseract-OCR/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Tesseract_OCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btOCR = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btOCR
+            // 
+            this.btOCR.Location = new System.Drawing.Point(13, 13);
+            this.btOCR.Name = "btOCR";
+            this.btOCR.Size = new System.Drawing.Size(75, 23);
+            this.btOCR.TabIndex = 0;
+            this.btOCR.Text = "OCR";
+            this.btOCR.UseVisualStyleBackColor = true;
+            this.btOCR.Click += new System.EventHandler(this.btOCR_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(13, 75);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(259, 175);
+            this.txtResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btOCR);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btOCR;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Tesseract-OCR/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace Tesseract_OCR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btOCR_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var img = new Bitmap(openFileDialog.FileName);
+                var ocr = new TesseractEngine("./tessdata", "eng", EngineMode.TesseractAndCube);
+                var page = ocr.Process(img);
+                txtResult.Text = page.GetText();
+            }
+        }
+    }
+}
